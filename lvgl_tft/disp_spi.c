5b7ae81450a0f774c99ba35c995f34e10cc6f1e1
Andrew Metcalf <andrew@stripe.com>
2024-02-25 08:47:59 -0800

    Move IRAM_ATTR to definition instead of prototype

--- lvgl_tft/disp_spi.c
+++ lvgl_tft/disp_spi.c
@@ -33,36 +33,36 @@
 
 /******************************************************************************
  * Notes about DMA spi_transaction_ext_t structure pooling
- * 
- * An xQueue is used to hold a pool of reusable SPI spi_transaction_ext_t 
- * structures that get used for all DMA SPI transactions. While an xQueue may 
- * seem like overkill it is an already built-in RTOS feature that comes at 
- * little cost. xQueues are also ISR safe if it ever becomes necessary to 
+ *
+ * An xQueue is used to hold a pool of reusable SPI spi_transaction_ext_t
+ * structures that get used for all DMA SPI transactions. While an xQueue may
+ * seem like overkill it is an already built-in RTOS feature that comes at
+ * little cost. xQueues are also ISR safe if it ever becomes necessary to
  * access the pool in the ISR callback.
- * 
- * When a DMA request is sent, a transaction structure is removed from the 
- * pool, filled out, and passed off to the esp32 SPI driver. Later, when 
- * servicing pending SPI transaction results, the transaction structure is 
- * recycled back into the pool for later reuse. This matches the DMA SPI 
+ *
+ * When a DMA request is sent, a transaction structure is removed from the
+ * pool, filled out, and passed off to the esp32 SPI driver. Later, when
+ * servicing pending SPI transaction results, the transaction structure is
+ * recycled back into the pool for later reuse. This matches the DMA SPI
  * transaction life cycle requirements of the esp32 SPI driver.
- * 
- * When polling or synchronously sending SPI requests, and as required by the 
- * esp32 SPI driver, all pending DMA transactions are first serviced. Then the 
- * polling SPI request takes place. 
- * 
- * When sending an asynchronous DMA SPI request, if the pool is empty, some 
- * small percentage of pending transactions are first serviced before sending 
- * any new DMA SPI transactions. Not too many and not too few as this balance 
+ *
+ * When polling or synchronously sending SPI requests, and as required by the
+ * esp32 SPI driver, all pending DMA transactions are first serviced. Then the
+ * polling SPI request takes place.
+ *
+ * When sending an asynchronous DMA SPI request, if the pool is empty, some
+ * small percentage of pending transactions are first serviced before sending
+ * any new DMA SPI transactions. Not too many and not too few as this balance
  * controls DMA transaction latency.
- * 
- * It is therefore not the design that all pending transactions must be 
- * serviced and placed back into the pool with DMA SPI requests - that 
- * will happen eventually. The pool just needs to contain enough to float some 
- * number of in-flight SPI requests to speed up the overall DMA SPI data rate 
- * and reduce transaction latency. If however a display driver uses some 
+ *
+ * It is therefore not the design that all pending transactions must be
+ * serviced and placed back into the pool with DMA SPI requests - that
+ * will happen eventually. The pool just needs to contain enough to float some
+ * number of in-flight SPI requests to speed up the overall DMA SPI data rate
+ * and reduce transaction latency. If however a display driver uses some
  * polling SPI requests or calls disp_wait_for_pending_transactions() directly,
  * the pool will reach the full state more often and speed up DMA queuing.
- * 
+ *
  *****************************************************************************/
 
 /*********************
@@ -73,7 +73,7 @@
 /* DMA Transactions to reserve before queueing additional DMA transactions. A 1/10th seems to be a good balance. Too many (or all) and it will increase latency. */
 #define SPI_TRANSACTION_POOL_RESERVE_PERCENTAGE 10
 #if SPI_TRANSACTION_POOL_SIZE >= SPI_TRANSACTION_POOL_RESERVE_PERCENTAGE
-#define SPI_TRANSACTION_POOL_RESERVE (SPI_TRANSACTION_POOL_SIZE / SPI_TRANSACTION_POOL_RESERVE_PERCENTAGE)	
+#define SPI_TRANSACTION_POOL_RESERVE (SPI_TRANSACTION_POOL_SIZE / SPI_TRANSACTION_POOL_RESERVE_PERCENTAGE)
 #else
 #define SPI_TRANSACTION_POOL_RESERVE 1	/* defines minimum size */
 #endif
@@ -85,7 +85,7 @@
 /**********************
  *  STATIC PROTOTYPES
  **********************/
-static void IRAM_ATTR spi_ready (spi_transaction_t *trans);
+static void spi_ready (spi_transaction_t *trans);
 
 /**********************
  *  STATIC VARIABLES
@@ -250,7 +250,7 @@ void disp_spi_transaction(const uint8_t *data, size_t length,
 		disp_wait_for_pending_transactions();	/* before synchronous queueing, all previous pending transactions need to be serviced */
         spi_device_transmit(spi, (spi_transaction_t *) &t);
     } else {
-		
+
 		/* if necessary, ensure we can queue new transactions by servicing some previous transactions */
 		if(uxQueueMessagesWaiting(TransactionPool) == 0) {
 			spi_transaction_t *presult;
@@ -297,7 +297,7 @@ void disp_spi_release(void)
  *   STATIC FUNCTIONS
  **********************/
 
-static void spi_ready(spi_transaction_t *trans)
+static void IRAM_ATTR spi_ready(spi_transaction_t *trans)
 {
     disp_spi_send_flag_t flags = (disp_spi_send_flag_t) trans->user;
 
@@ -322,4 +322,3 @@ static void spi_ready(spi_transaction_t *trans)
         chained_post_cb(trans);
     }
 }
-
